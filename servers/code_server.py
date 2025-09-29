18d4660b8243e1f121fdf40703fee59cad7a108b
eliezerraj <eliezerraj@gmail.com>
2025-09-29 01:17:48 -0300

    feat:new tool

--- servers/code_server.py
+++ servers/code_server.py
@@ -30,7 +30,8 @@ async def gateway_grpc_healthy(context: dict = None) -> str:
     Raises:
         - valueError: http status code
     """
-    
+    logger.info(f"function => gateway_grpc_healthy()")
+
     jwt_token = context.get("jwt") if context else None
     if not jwt_token:
         logger.error( "No JWT provided, NOT AUTHORIZED, statuscode: 403")
@@ -61,6 +62,8 @@ async def payment_gateway_healthy(context: dict = None) -> str:
         - valueError: http status code
     """
     
+    logger.info(f"function => payment_gateway_healthy()")
+    
     jwt_token = context.get("jwt") if context else None
     if not jwt_token:
         logger.error( "No JWT provided, NOT AUTHORIZED, statuscode: 403")
@@ -78,7 +81,44 @@ async def payment_gateway_healthy(context: dict = None) -> str:
                 return f"{data}"
             else:
                 return f"Failed to fetch payment_gateway healthy, statuscode: {resp.status}"
-            
+
+@mcp.tool(name="get_card_payment")
+async def get_card_payment(card: str, date: str, context: dict = None) -> str:
+    """
+    Get all payments did by a CREDIT CARD or a DEBIT CARD such as payments (amount, date, type and model), card status, atc, mcc, holder
+
+    Args:
+        - card: card id with a strictly in the format: 999.999.999.999 (Exactly 12 digits split into 4 groups of 3 digits each)
+        - card: payment date format YYYY-MM-DD
+    Response:
+        - list: A list of payments with information such as card type, card model, payment amount, terminal, payment status and payment date
+    Raises:
+        - valueError: http status code
+    """
+    
+    logger.info(f"function => get_card_payment() card:{card} date:{date} ")
+    
+    jwt_token = context.get("jwt") if context else None
+    if not jwt_token:
+        logger.error( "No JWT provided, NOT AUTHORIZED, statuscode: 403")
+        return "No JWT provided, NOT AUTHORIZED, statuscode: 403"
+ 
+    logger.info(f"jwt_token: {jwt_token}")
+    
+    headers = {"Authorization": f"Bearer {jwt_token}"}                  
+    url = f"https://go-global-apex.architecture.caradhras.io/payment-gateway/payment?card={card}&after={date}"
+    
+    async with aiohttp.ClientSession(timeout=session_timeout) as session:
+        async with session.get(url, headers=headers) as resp:
+            if resp.status == 200:
+                data = await resp.json()
+
+                print(f"data =========> {data}")
+                
+                return f"{data}"
+            else:
+                return f"Failed to fetch payment_gateway healthy, statuscode: {resp.status}"
+                        
 # Limit
 @mcp.tool(name="limit_healthy")
 async def limit_healthy(context: dict = None) -> str:
@@ -91,6 +131,8 @@ async def limit_healthy(context: dict = None) -> str:
         - valueError: http status code
     """
     
+    logger.info(f"function => limit_healthy()")
+
     jwt_token = context.get("jwt") if context else None
     if not jwt_token:
         logger.error( "No JWT provided, NOT AUTHORIZED, statuscode: 403")
@@ -120,7 +162,9 @@ async def card_healthy(context: dict = None) -> str:
     Raises:
         - valueError: http status code
     """
-    
+
+    logger.info(f"function => card_healthy()")
+
     jwt_token = context.get("jwt") if context else None
     if not jwt_token:
         logger.error( "No JWT provided, NOT AUTHORIZED, statuscode: 403")
@@ -152,7 +196,9 @@ async def get_card(card: str, context: dict = None) -> str:
     Raises:
         - valueError: http status code
     """
-    
+
+    logger.info(f"function => get_card() = card:{card}")
+
     jwt_token = context.get("jwt") if context else None
     if not jwt_token:
         logger.error( "No JWT provided, NOT AUTHORIZED, statuscode: 403")
@@ -171,7 +217,6 @@ async def get_card(card: str, context: dict = None) -> str:
             else:
                 return f"Failed to fetch card from {card}, statuscode: {resp.status}"
 
-
 # Account
 @mcp.tool(name="account_healthy")
 async def account_healthy(context: dict = None) -> str:
@@ -183,7 +228,9 @@ async def account_healthy(context: dict = None) -> str:
     Raises:
         - valueError: http status code
     """
-    
+
+    logger.info(f"function => account_healthy()")
+
     jwt_token = context.get("jwt") if context else None
     if not jwt_token:
         logger.error( "No JWT provided, NOT AUTHORIZED, statuscode: 403")
@@ -214,7 +261,9 @@ async def get_account(account: str, context: dict = None) -> str:
     Raises:
         - valueError: http status code
     """
-    
+
+    logger.info(f"function => get_account() = account: {account}")
+
     jwt_token = context.get("jwt") if context else None
     if not jwt_token:
         logger.error( "No JWT provided, NOT AUTHORIZED, statuscode: 403")
@@ -245,7 +294,9 @@ async def get_accounts_from_person(person: str, context: dict = None) -> str:
     Raises:
         - valueError: http status code
     """
-    
+
+    logger.info(f"function => get_accounts_from_person() = person: {person}")
+
     jwt_token = context.get("jwt") if context else None
     if not jwt_token:
         logger.error( "No JWT provided, NOT AUTHORIZED, statuscode: 403")
@@ -276,6 +327,7 @@ async def ledger_healthy(context: dict = None) -> str:
     Raises:
         - valueError: http status code
     """
+    logger.info(f"function => ledger_healthy()")
     
     jwt_token = context.get("jwt") if context else None
     if not jwt_token:
@@ -308,6 +360,8 @@ async def get_account_statement(account: str, context: dict = None) -> str:
         - valueError: http status code
     """
     
+    logger.info(f"function => get_account_statement() = account: {account}")
+    
     jwt_token = context.get("jwt") if context else None
     if not jwt_token:
         logger.error( "No JWT provided, NOT AUTHORIZED, statuscode: 403")
@@ -331,12 +385,12 @@ async def get_account_statement(account: str, context: dict = None) -> str:
 @mcp.tool(name="store_memory_graph_account")
 async def store_memory_graph_account(person: str, account: str, relation: str, context: dict = None) -> str:
     """
-    Store all accounts informations from a endpoint via rest call api
+    Store the ACCOUNT its relation with PERSON in memory graph account
 
     Args:
         - account: account identificator
         - person: person identificator
-        = relations: relation between account and person
+        - relations: relation between account and person, this relation MUST BE 'HAS'
     Response:
         - account: account identificator
         - person: person identificator
@@ -345,15 +399,30 @@ async def store_memory_graph_account(person: str, account: str, relation: str, c
         - valueError: http status code
     """
 
+    logger.info(f"function => store_memory_graph_account() = person: {person} account: {account} relation: {relation}")
+
     payload = {
-        "person": {"person_id": person},
-        "account": {"account_id": account},
-        "relations": {"description": relation},
+        "nodes": {
+            "person": {
+                "person_id": person
+            },
+            "account": {
+                "account_id": account
+                }
+            },
+        "relations": {
+            "description": relation
+        },
     }
 
-    headers = {"Authorization": f"Bearer {TOKEN}",
-               "Content-Type": "application/json",
-            }
+    jwt_token = context.get("jwt") if context else None
+    if not jwt_token:
+        logger.error( "No JWT provided, NOT AUTHORIZED, statuscode: 403")
+        return "No JWT provided, NOT AUTHORIZED, statuscode: 403"
+ 
+    logger.info(f"jwt_token: {jwt_token}")
+    
+    headers = {"Authorization": f"Bearer {jwt_token}"}   
     
     url = f"http://localhost:8001/graph"
 
@@ -369,17 +438,27 @@ async def store_memory_graph_account(person: str, account: str, relation: str, c
 @mcp.tool(name="retrieve_memory_graph_account")
 async def retrieve_memory_graph_account(account: str, context: dict = None) -> str:
     """
-    Retrive from memory graph accounts and their informations from a endpoint via rest call api
+    Retrive from memory graph all account informations
 
     Args:
         - account: account identificator
     Response:
-        - list: list of person_id
+        - list: list of person_id (owner) of account
     Raises:
         - valueError: http status code
     """
 
-    headers = {"Authorization": f"Bearer {TOKEN}" }
+    logger.info(f"function => retrieve_memory_graph_account() = account: {account}")
+
+    jwt_token = context.get("jwt") if context else None
+    if not jwt_token:
+        logger.error( "No JWT provided, NOT AUTHORIZED, statuscode: 403")
+        return "No JWT provided, NOT AUTHORIZED, statuscode: 403"
+ 
+    logger.info(f"jwt_token: {jwt_token}")
+    
+    headers = {"Authorization": f"Bearer {jwt_token}"}   
+
     url = f"http://localhost:8001/person/account/{account}"
 
     async with aiohttp.ClientSession(timeout=session_timeout) as session:
@@ -390,7 +469,116 @@ async def retrieve_memory_graph_account(account: str, context: dict = None) -> s
                 return f"{data}"
             else:
                 return f"Failed to get data from {account}, statuscode: {resp.status}"
+
+@mcp.tool(name="store_memory_graph_card")
+async def store_memory_graph_card(card: str, account: str, relation: str, context: dict = None) -> str:
+    """
+    Store the CARD its relation with ACCOUNT in memory graph account
+
+    Args:
+        - card: card identificator
+        - account: account identificator
+        - relations: relation between card and account, this relation MUST BE 'ISSUED'
+    Response:
+        - card: card identificator
+        - account: account identificator
+        - relations: relation between card and account
+    Raises:
+        - valueError: http status code
+    """
+
+    logger.info(f"function => store_memory_graph_card() = card: {card} account: {account} relation: {relation}")
+
+    payload = {
+        "nodes": {
+            "card": {
+                "card_id": card
+            },
+            "account": {
+                "account_id": account
+                }
+            },
+        "relations": {
+            "description": relation
+        },
+    }
+
+    jwt_token = context.get("jwt") if context else None
+    if not jwt_token:
+        logger.error( "No JWT provided, NOT AUTHORIZED, statuscode: 403")
+        return "No JWT provided, NOT AUTHORIZED, statuscode: 403"
+ 
+    logger.info(f"jwt_token: {jwt_token}")
+    
+    headers = {"Authorization": f"Bearer {jwt_token}"}   
+    
+    url = f"http://localhost:8001/graph"
+
+    async with aiohttp.ClientSession(timeout=session_timeout) as session:
+        async with session.post(url, headers=headers, json=payload) as resp:
             
+            if resp.status == 200:
+                data = await resp.json()
+                return f"{data}"
+            else:
+                return f"Failed to post data {account}, statuscode: {resp.status}"
+
+@mcp.tool(name="store_memory_graph_card_payment")
+async def store_memory_graph_card_payment(card: str, payment: str, relation: str, context: dict = None) -> str:
+    """
+    Store the PAYMENT its relation with CARD in memory graph account
+
+    Args:
+        - card: card identificator
+        - payment: payment identificator
+        - relations: relation between card and payment, this relation MUST BE 'PAY'
+    Response:
+        - card: card identificator
+        - payment: payment identificator
+        - relations: relation between card and account
+    Raises:
+        - valueError: http status code
+    """
+
+    logger.info(f"function => store_memory_graph_card_payment() = card: {card} account: {payment} relation: {relation}")
+
+    payload = {
+        "nodes": {
+            "card": {
+                "card_id": card
+            },
+            "payment": {
+                "payment_id": payment
+                }
+            },
+        "relations": {
+            "description": relation
+        },
+    }
+
+    jwt_token = context.get("jwt") if context else None
+    if not jwt_token:
+        logger.error( "No JWT provided, NOT AUTHORIZED, statuscode: 403")
+        return "No JWT provided, NOT AUTHORIZED, statuscode: 403"
+ 
+    logger.info(f"jwt_token: {jwt_token}")
+    
+    headers = {"Authorization": f"Bearer {jwt_token}"}   
+    
+    url = f"http://localhost:8001/graph"
+
+    async with aiohttp.ClientSession(timeout=session_timeout) as session:
+        async with session.post(url, headers=headers, json=payload) as resp:
+            
+            if resp.status == 200:
+                data = await resp.json()
+                return f"{data}"
+            else:
+                return f"Failed to post data {account}, statuscode: {resp.status}"
+
+
+
+
 if __name__ == "__main__":
     print("-" * 45)
     print(f"CODE SERVER {HOST}:{PORT}")
